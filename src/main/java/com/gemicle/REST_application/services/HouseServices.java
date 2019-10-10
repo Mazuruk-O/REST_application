74fee96bdbe0e3f4edcb5939189a47f79dbbf617
Oleg Mazuruk <mbeta75@gmail.com>
2019-10-10 12:51:52 +0300

    add house services

---
+++ src/main/java/com/gemicle/REST_application/services/HouseServices.java
@@ -0,0 +1,17 @@
+package com.gemicle.REST_application.services;
+
+import com.gemicle.REST_application.model.House;
+
+import java.util.List;
+
+public interface HouseServices {
+    void save(House entity);
+
+    House findByYearConstruction(Long yearConstruction);
+
+    House findByAddress(String address);
+
+    List<House> findAll();
+
+    void delete(House entity);
+}
---
+++ src/main/java/com/gemicle/REST_application/services/HouseServicesImpl.java
@@ -0,0 +1,49 @@
+package com.gemicle.REST_application.services;
+
+import com.gemicle.REST_application.model.House;
+import com.gemicle.REST_application.model.HouseImpl;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class HouseServicesImpl implements HouseServices {
+    private List<House> houses = new LinkedList<>();
+
+    {
+        houses.add(new HouseImpl("Soborna 52",5,6,7,1998));
+        houses.add(new HouseImpl("Chornovola 88",10,10,9,2012));
+        houses.add(new HouseImpl("Kyivska 46",7,8,9,2015));
+    }
+
+    @Override
+    public void save(House entity) {
+        if(!houses.contains(entity))
+            houses.add(entity);
+    }
+
+    @Override
+    public House findByYearConstruction(final Long yearConstruction) {
+        return houses.stream()
+                .filter(house -> yearConstruction == house.getYearConstruction())
+                .findAny()
+                .orElse(null);
+    }
+
+    @Override
+    public House findByAddress(final String address) {
+        return houses.stream()
+                .filter(house -> address.equals(house.getAddress()))
+                .findAny()
+                .orElse(null);
+    }
+
+    @Override
+    public List<House> findAll() {
+        return houses;
+    }
+
+    @Override
+    public void delete(House entity) {
+        houses.remove(entity);
+    }
+}
