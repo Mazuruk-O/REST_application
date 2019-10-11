0940eb9a394c283a7231d435c15d0c4aaf007e8d
Oleg Mazuruk <mbeta75@gmail.com>
2019-10-11 15:05:10 +0300

    fix main

--- src/main/java/com/gemicle/REST_application/Main.java
+++ src/main/java/com/gemicle/REST_application/Main.java
@@ -1,7 +1,13 @@
 package com.gemicle.REST_application;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
 public class Main {
     public static void main(String[] args) {
-
+        SpringApplication.run(Main.class, args);
     }
 }
