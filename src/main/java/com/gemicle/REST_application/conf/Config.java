1e896fed9c41fffda208d45c879791cd8ca8bd6b
Oleg <mbeta75@gmail.com>
2019-10-22 10:57:16 +0300

    add filter for multipart

--- src/main/java/com/gemicle/REST_application/conf/Config.java
+++ src/main/java/com/gemicle/REST_application/conf/Config.java
@@ -1,5 +1,7 @@
 package com.gemicle.REST_application.conf;
 
+import com.gemicle.REST_application.filter.MultipartFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
@@ -7,11 +9,14 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 @Configuration
 public class Config {
 
-    @Bean(name = "multipartResolver")
-    public CommonsMultipartResolver multipartResolver()
-    {
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-        multipartResolver.setMaxUploadSize(20848820);
-        return multipartResolver;
+    @Bean
+    public FilterRegistrationBean<MultipartFilter> loggingFilter(){
+        FilterRegistrationBean<MultipartFilter> registrationBean
+                = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new MultipartFilter());
+        registrationBean.addUrlPatterns("/acceptImageAndDataHouse/*");
+
+        return registrationBean;
     }
 }
--- src/main/java/com/gemicle/REST_application/controller/MultipartController.java
+++ src/main/java/com/gemicle/REST_application/controller/MultipartController.java
@@ -1,10 +1,36 @@
 package com.gemicle.REST_application.controller;
 
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gemicle.REST_application.model.House;
+import com.gemicle.REST_application.model.HouseImpl;
+import com.gemicle.REST_application.services.HouseServices;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 @RestController
-@PropertySource("classpath:application.properties")
 public class MultipartController {
+    private static ObjectMapper mapper = new ObjectMapper();
+    @Autowired
+    private HouseServices houseServices;
 
+    @PostMapping(value = "/acceptImageAndDataHouse", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public @ResponseBody String uploadImageAndDataHouse(@RequestPart(value = "imgPart") MultipartFile imageFile
+                                         , @RequestPart(value = "houseData",required = false) HouseImpl houseData) throws IOException {
+
+
+        File outputfile = new File("saved.jpeg");
+
+        ImageIO.write(ImageIO.read(imageFile.getInputStream()), "jpeg", outputfile);
+
+        //House houseParseJSON = mapper.readValue(houseData, HouseImpl.class);
+
+        return houseServices.save(houseData).toString();
+    }
 }
