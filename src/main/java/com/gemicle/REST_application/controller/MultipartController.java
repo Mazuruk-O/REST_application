917024f8f4f1246602e9fb15be7a6fcfb20b18ff
Oleg <mbeta75@gmail.com>
2019-10-22 12:07:34 +0300

    fix multipart controller

--- src/main/java/com/gemicle/REST_application/controller/MultipartController.java
+++ src/main/java/com/gemicle/REST_application/controller/MultipartController.java
@@ -2,15 +2,19 @@ package com.gemicle.REST_application.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gemicle.REST_application.model.House;
+import com.gemicle.REST_application.model.HouseDTO;
 import com.gemicle.REST_application.model.HouseImpl;
 import com.gemicle.REST_application.services.HouseServices;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
+import javax.print.attribute.standard.MediaSize;
 import java.io.File;
 import java.io.IOException;
 
@@ -21,7 +25,7 @@ public class MultipartController {
     private HouseServices houseServices;
 
     @PostMapping(value = "/acceptImageAndDataHouse", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public @ResponseBody String uploadImageAndDataHouse(@RequestPart(value = "imgPart") MultipartFile imageFile
+    public ResponseEntity<?> uploadImageAndDataHouse(@RequestPart(value = "imageFile") MultipartFile imageFile
                                          , @RequestPart(value = "houseData",required = false) HouseImpl houseData) throws IOException {
 
 
@@ -29,8 +33,23 @@ public class MultipartController {
 
         ImageIO.write(ImageIO.read(imageFile.getInputStream()), "jpeg", outputfile);
 
-        //House houseParseJSON = mapper.readValue(houseData, HouseImpl.class);
-
-        return houseServices.save(houseData).toString();
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON)
+                .body(houseServices.save(houseData));
     }
+
+    @PostMapping(value = "/acceptDTOHouse")
+    public ResponseEntity<?> acceptDTOHouse(@ModelAttribute HouseDTO houseDTO){
+        try {
+            File outputfile = new File("savedFromDTO.jpeg");
+
+            ImageIO.write(ImageIO.read(houseDTO.getImageFile().getInputStream()), "jpeg", outputfile);
+
+            houseServices.save(houseDTO.getHouseData());
+
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(houseServices.save(houseDTO.getHouseData()));
+    }
+
 }
--- web/WEB-INF/pages/fileUpload.jsp
+++ web/WEB-INF/pages/fileUpload.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%--
   Created by IntelliJ IDEA.
   User: mbeta
@@ -11,25 +12,15 @@
     <title>Spring REST File Upload</title>
 </head>
 <body>
-<form method="POST" action="/SpringRestExample/api/rest/employee-management/employees/1/photo/multiple"
-      enctype="multipart/form-data">
-    <table>
-        <tr>
-            <td>Select first file to upload</td>
-            <td><input type="file" name="files" /></td>
-        </tr>
-        <tr>
-            <td>Select second file to upload</td>
-            <td><input type="file" name="files" /></td>
-        </tr>
-        <tr>
-            <td>Select third file to upload</td>
-            <td><input type="file" name="files" /></td>
-        </tr>
-        <tr>
-            <td><input type="submit" value="Submit" /></td>
-        </tr>
-    </table>
-</form>
+<form:form method="POST" action="/spring-mvc-basics/addEmployee"
+           modelAttribute="employee">
+    <form:label path="name">Name</form:label>
+    <form:input path="name" />
+
+    <form:label path="id">Id</form:label>
+    <form:input path="id" />
+
+    <input type="submit" value="Submit" />
+</form:form>
 </body>
 </html>
