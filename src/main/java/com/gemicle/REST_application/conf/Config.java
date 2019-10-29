aa816d483a0c46183836deff1f2cde6ab4401068
Oleg <mbeta75@gmail.com>
2019-10-29 14:48:43 +0200

    add MongoDB

--- src/main/java/com/gemicle/REST_application/conf/Config.java
+++ src/main/java/com/gemicle/REST_application/conf/Config.java
@@ -1,5 +1,10 @@
 package com.gemicle.REST_application.conf;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.gemicle.REST_application.filter.MultipartFilter;
+import com.gemicle.REST_application.model.HouseImpl;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
@@ -7,11 +12,29 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 @Configuration
 public class Config {
 
-    @Bean(name = "multipartResolver")
-    public CommonsMultipartResolver multipartResolver()
-    {
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-        multipartResolver.setMaxUploadSize(20848820);
-        return multipartResolver;
+    @Bean
+    public FilterRegistrationBean<MultipartFilter> loggingFilter(){
+        FilterRegistrationBean<MultipartFilter> registrationBean
+                = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new MultipartFilter());
+        registrationBean.addUrlPatterns("/acceptImageAndDataHouse/*");
+
+        return registrationBean;
+    }
+
+    @Bean
+    public ObjectMapper createOojectWrapper(){
+        ObjectMapper mapper = new ObjectMapper();
+
+        /*SimpleModule module = new SimpleModule();
+
+        module.addDeserializer(HouseImpl.class, new HouseDeserializer()); // register custom Deserializer
+
+        module.addSerializer(HouseImpl.class, new HouseSerializer()); // register custom Serializer
+
+        mapper.registerModule(module);*/
+
+        return mapper;
     }
 }
